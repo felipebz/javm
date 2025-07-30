46c37e82b84841cdcdfd3ead49170e5e0789bf39
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2025-07-29 21:37:57 -0300

    refactor: fix javm.ps1 to work as intended (closes #14)

--- command/shellscripts/javm.ps1
+++ command/shellscripts/javm.ps1
@@ -1,13 +1,16 @@
-$global:_javmExecutable = '::JAVM::'
-
-function javm
+function global:javm
 {
-    $fd3=$([System.IO.Path]::GetTempFileName())
-    & $global:_javmExecutable $args --fd3 "$fd3"
-    $fd3content=$(Get-Content $fd3)
-    if ($fd3content) {
-        $expression=$fd3content.replace("export ","`$env:").replace("unset ","Remove-Item env:") -join "`n"
-        if (-not $expression -eq "") { Invoke-Expression $expression }
+    $javmExecutable = '::JAVM::'
+    $fd3 = [System.IO.Path]::GetTempFileName()
+    & $javmExecutable $args --fd3 "$fd3"
+    $fd3content = Get-Content $fd3
+    if ($fd3content)
+    {
+        $expression = $fd3content.replace("export ", "`$env:").replace("unset ", "Remove-Item env:") -join "`n"
+        if ($expression -ne "")
+        {
+            Invoke-Expression $expression
+        }
     }
     Remove-Item -Force $fd3
 }
