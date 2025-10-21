1a9b1df47fc547d3307758e2745ccdb3a8a8cae7
Felipe Zorzo <felipe.b.zorzo@gmail.com>
2025-10-20 21:41:25 -0300

    feat(packages): support musl-based Linux distributions (closes #9)

--- discoapi/packages.go
+++ discoapi/packages.go
@@ -1,9 +1,14 @@
 package discoapi
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/url"
+	"os/exec"
+	"runtime"
+
+	log "github.com/sirupsen/logrus"
 )
 
 func (c *Client) GetPackages(os, arch, distribution, version string) ([]Package, error) {
@@ -26,12 +31,19 @@ func (c *Client) GetPackages(os, arch, distribution, version string) ([]Package,
 		params.Set("lib_c_type", "c_std_lib")
 	} else {
 		params.Set("archive_type", "tar.gz")
-		params.Set("lib_c_type", "glibc") // TODO support musl based distros
+		// Default to glibc for non-Windows, override to musl when detected at runtime on Linux
+		libc := "glibc"
+		if runtime.GOOS == "linux" && isMuslLibc() {
+			libc = "musl"
+		}
+		log.Debugf("OS is %s, libc is %s", runtime.GOOS, libc)
+		params.Set("lib_c_type", libc)
 	}
 
 	params.Set("package_type", "jdk")
 	params.Set("release_status", "ga")
 
+	log.Debugf("fetching packages with params: %s", params.Encode())
 	data, err := c.fetch("packages", params)
 	if err != nil {
 		return nil, fmt.Errorf("failed to fetch packages: %w", err)
@@ -43,6 +55,14 @@ func (c *Client) GetPackages(os, arch, distribution, version string) ([]Package,
 	}
 
 	return resp.Packages, nil
+}
+
+// isMuslLibc attempts to detect musl-based Linux by inspecting `ldd --version` output.
+// It best-effort returns false if the command is unavailable or any error occurs.
+func isMuslLibc() bool {
+	cmd := exec.Command("ldd", "--version")
+	out, _ := cmd.CombinedOutput()
+	return bytes.Contains(out, []byte("musl"))
 }
 
 func (c *Client) GetPackageInfo(id string) (*PackageInfo, error) {
